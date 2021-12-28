f09cac68b3d1b07c18839ec3ba4243e516820f62
dobryivecher1636 <94709490+dobryivecher1636@users.noreply.github.com>
2021-12-28 22:19:53 +0500

    Add files via upload

---
+++ lab9_mediator/mediator/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace mediator
+{
+    class Program
+    {
+        abstract class Mediator //интерфейс взаимодействия с объектами Partner
+        {
+            public abstract void Send(string message, Partner employee);
+        }
+
+        abstract class Partner //интерфейс взаимодействия с объектами Mediator
+        {
+            public Mediator mediator;
+            public Partner(Mediator mediator)
+            {
+                this.mediator = mediator;
+            }
+            public virtual void Send(string message)
+            {
+                mediator.Send(message, this);
+            }
+            public abstract void Notice(string message);
+        }
+
+        class Client : Partner //заказчик
+        {
+            public Client(Mediator mediator)
+            : base(mediator)
+            {}
+            public override void Notice(string message)
+            {
+                Console.WriteLine("Сообщение заказчику: " + message);
+            }
+        }
+
+        class Programmer : Partner //программист
+        {
+            public Programmer(Mediator mediator)
+            : base(mediator)
+            {}
+            public override void Notice(string message)
+            {
+                Console.WriteLine("Сообщение программисту: " + message);
+            }
+        }
+
+        class Tester : Partner //тестер
+        {
+            public Tester(Mediator mediator)
+            : base(mediator)
+            {}
+            public override void Notice(string message)
+            {
+                Console.WriteLine("Сообщение тестеру: " + message);
+            }
+        }
+
+        class ManagerMediator : Mediator
+        {
+            public Partner Customer {get; set;}
+            public Partner Programmer {get; set;}
+            public Partner Tester {get; set;}
+            public override void Send(string message, Partner employee)
+            {
+                if (employee == Customer) //если заказчик, то отправляем сообщение программисту
+                    Programmer.Notice(message);
+                if (employee == Programmer) //если программист, то отправляем сообщение тестеру
+                    Tester.Notice(message);
+                if (employee == Tester) //если тестер, то отправляем сообщение заказчику
+                    Customer.Notice(message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ManagerMediator mediator = new ManagerMediator();
+            Partner customer = new Client(mediator);
+            Partner tester = new Tester(mediator);
+            Partner programmer = new Programmer(mediator);
+
+            mediator.Customer = customer;
+            mediator.Tester = tester;
+            mediator.Programmer = programmer;
+
+            customer.Send("Я хочу заказать разработку программы.");
+            programmer.Send("Программа готова.");
+            tester.Send("Программа протестирована, заказ готов.");
+        }
+    }
+
+}
