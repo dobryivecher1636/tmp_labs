b3a5506188253525b6b61128adfeab8e29610912
dobryivecher1636 <94709490+dobryivecher1636@users.noreply.github.com>
2021-12-28 22:21:19 +0500

    Add files via upload

---
+++ lab9_state/state/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace state
+{
+    class Water
+    {
+        public IWaterState State {get; set;}
+
+        public Water(IWaterState wstate)
+        {
+            State = wstate;
+        }
+
+        public void Heat()
+        {
+            State.Heat(this);
+        }
+
+        public void Frost()
+        {
+            State.Frost(this);
+        }
+    }
+
+    interface IWaterState
+    {
+        void Heat(Water water);
+        void Frost(Water water);
+    }
+
+    class IceWaterState : IWaterState
+    {
+        public void Heat(Water water)
+        {
+            Console.WriteLine("Превращаем лед в жидкость");
+            water.State = new LiquidWaterState();
+        }
+
+        public void Frost(Water water)
+        {
+            Console.WriteLine("Продолжаем заморозку льда");
+        }
+    }
+
+    class LiquidWaterState : IWaterState
+    {
+        public void Heat(Water water)
+        {
+            Console.WriteLine("Превращаем жидкость в пар");
+            water.State = new GasWaterState();
+        }
+
+        public void Frost(Water water)
+        {
+            Console.WriteLine("Превращаем жидкость в лед");
+            water.State = new IceWaterState();
+        }
+    }
+
+    class GasWaterState : IWaterState
+    {
+        public void Heat(Water water)
+        {
+            Console.WriteLine("Повышаем температуру пара");
+        }
+
+        public void Frost(Water water)
+        {
+            Console.WriteLine("Превращаем пар в жидкость");
+            water.State = new LiquidWaterState();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Water water = new Water(new LiquidWaterState());
+            water.Frost();
+            water.Frost();
+        }
+    }
+}
