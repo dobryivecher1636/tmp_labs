9a1173ac8171886952d4ff08be02399a78d05dee
dobryivecher1636 <94709490+dobryivecher1636@users.noreply.github.com>
2021-12-28 22:20:49 +0500

    Add files via upload

---
+++ lab9_singletone/singletone/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace singletone
+{
+    class Program
+    {
+        class Game
+        {
+            public Ball ball {get; set;}
+            public void IsOnTheCourt()
+            {
+                ball = Ball.BallToTheCourt();
+            }
+        }
+        class Ball
+        {
+            private static Ball ball;
+            public static Ball BallToTheCourt()
+            {
+                if (ball == null)
+                    ball = new Ball();
+                else
+                    Console.WriteLine("Мяч уже на площадке");
+                return ball;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            game.IsOnTheCourt(); //мяч вводится в игру на площадке
+            game.IsOnTheCourt(); //выведет сообщение о том, что мяч уже на площадке
+        }
+    }
+}
